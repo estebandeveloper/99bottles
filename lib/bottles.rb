5aa8413789c73798dae9954761722132af626c3f
Kyren <dev.esteban@gmail.com>
2017-04-11 16:20:34 -0300

    third test done

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,10 @@
 class Bottles
   def verse(x)
+    remaining = (x-1) == 1 ? "#{x-1} bottle" : "#{x-1} bottles"
+
     result = <<-VERSE
 #{x} bottles of beer on the wall, #{x} bottles of beer.
-Take one down and pass it around, #{x-1} bottles of beer on the wall.
+Take one down and pass it around, #{remaining} of beer on the wall.
 VERSE
   end
 end
